5c32fa30b6756ef8a494db43bf7643111c39e065
Tomython <dmitrymacpro@gmail.com>
2025-02-21 19:34:34 +0300

    Докер поменял

--- obj/Debug/net8.0/XtalPlayer.AssemblyInfo.cs
+++ obj/Debug/net8.0/XtalPlayer.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("XtalPlayer")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+187a7dc44758c3c8bf8787aee6ec8294941c68bd")]
 [assembly: System.Reflection.AssemblyProductAttribute("XtalPlayer")]
 [assembly: System.Reflection.AssemblyTitleAttribute("XtalPlayer")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
